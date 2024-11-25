20ae84ffb653513c09d56ded2dcf39796867130b
Kevin <133233113+kevinmlisboa@users.noreply.github.com>
2024-11-25 09:26:29 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         NewsAgency agency = new NewsAgency();
-        Subscriber sub1 = new Subscriber("Alice");
-        Subscriber sub2 = new Subscriber("Bob");
-        Subscriber sub3 = new Subscriber("Charlie");
+        Subscriber sub1 = new Subscriber("Kevin");
+        Subscriber sub2 = new Subscriber("Ros");
+        Subscriber sub3 = new Subscriber("Lisboa");
         agency.subscribe(sub1);
         agency.subscribe(sub2);
         agency.publishNews("New Java release is out!");
